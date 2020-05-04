d4abc1d0af82c8736ba241b08fe1e49b6724ccd2
unknown <97lynk@gmail.com>
2020-05-04 14:37:33 +0700

    add gateway

---
+++ spring-cloud-gateway/src/test/java/io/a97lynk/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.a97lynk.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
